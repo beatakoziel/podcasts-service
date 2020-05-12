5fc37d567933d0bddcd4da14b977d55ba3fcc6f9
Beata Kozieł <beatainez.k@gmail.com>
2020-05-12 17:40:32 +0200

    podcast length

--- src/main/java/com/purplecast/podcasts/db/entity/Podcast.java
+++ src/main/java/com/purplecast/podcasts/db/entity/Podcast.java
@@ -37,4 +37,7 @@ public class Podcast {
 
     private BigDecimal price;
 
+    @Column(nullable = false)
+    private String length;
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
 alter table podcast CHANGE COLUMN audio_url audio_url VARCHAR(1000);
-insert into podcast (id, title, description, category, image_url, audio_url, blocked)
+insert into podcast (id, title, description, category, image_url, audio_url, blocked, length)
 values (1,
         'Walka z kryzysem',
         'Jak radzić sobie finansowo podczas kryzysu, który w tym momencie już ma miejsce?',
         'money',
         'https://images.unsplash.com/photo-1527788263495-3518a5c1c42d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1083&q=80',
         'metody_zmiany_nawykow',
-        false) ON DUPLICATE KEY UPDATE title='Walka z kryzysem';
-insert into podcast (id, title, description, category, image_url, audio_url, blocked, price)
+        false,
+        '04:06') ON DUPLICATE KEY UPDATE title='Walka z kryzysem';
+insert into podcast (id, title, description, category, image_url, audio_url, blocked, price, length)
 values (2,
         'Metody zmiany nawyków',
         'W świecie rozwoju osobistego nawyki są największym narzędziem zmiany czegokolwiek.',
@@ -15,8 +16,9 @@ values (2,
         'https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         'metody_zmiany_nawykow',
         true,
-        5) ON DUPLICATE KEY update title='Metody zmiany nawyków';
-insert into podcast (id, title, description, category, image_url, audio_url, blocked, price)
+        5,
+        '16:24') ON DUPLICATE KEY update title='Metody zmiany nawyków';
+insert into podcast (id, title, description, category, image_url, audio_url, blocked, price, length)
 values (3,
         'Jaką cenę zapłacimy',
         'O globalnym ociepleniu opowiada fizyk i ekspert ds. zmian klimatycznych Marcin Popkiewicz.',
@@ -24,8 +26,9 @@ values (3,
         'https://images.unsplash.com/photo-1517925035435-7976539b920d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
         'globalne_ocieplenie',
         true,
-        1) ON DUPLICATE KEY update title='Jaką cenę zapłacimy';
-insert into podcast (id, title, description, category, image_url, audio_url, blocked, price)
+        1,
+        '12:01') ON DUPLICATE KEY update title='Jaką cenę zapłacimy';
+insert into podcast (id, title, description, category, image_url, audio_url, blocked, price, length)
 values (4,
         'Książkowy rozum',
         'Jak książki kreują nasze spojrzenie na świat? Przewaga książek nad telewizją?',
@@ -33,8 +36,9 @@ values (4,
         'https://images.unsplash.com/photo-1512474331201-782fc6a4ee29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         'metody_zmiany_nawykow',
         true,
-        2) ON DUPLICATE KEY update title='Książkowy rozum';
-insert into podcast (id, title, description, category, image_url, audio_url, blocked, price)
+        2,
+        '11:11') ON DUPLICATE KEY update title='Książkowy rozum';
+insert into podcast (id, title, description, category, image_url, audio_url, blocked, price, length)
 values (5,
         'Znajdź sobie hobby',
         'O tym jak ważne jest hobby w rozwoju samego siebie i relacjach z innymi..',
@@ -42,6 +46,7 @@ values (5,
         'https://images.unsplash.com/photo-1522410818928-5522dacd5066?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         'metody_zmiany_nawykow',
         true,
-        2) ON DUPLICATE KEY update title='Znajdź sobie hobby';
+        2,
+        '12:00') ON DUPLICATE KEY update title='Znajdź sobie hobby';
 
 replace into podcast_user(id, username, password) values (1, 'admin', '$2y$12$b5LIwLw1C39dptntq1atSu/A2YhCXYWxiPLou/yT5DgKtVaYji3TG');
